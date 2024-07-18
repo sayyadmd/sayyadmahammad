ab9911d54e69729eac5f86cf684b1e7780fbafac
Sayyad Mahammad <sayyadmahammad@aesl.in>
2024-07-18 23:42:26 +0530

    site name update

--- app/page.js
+++ app/page.js
@@ -1,7 +1,13 @@
+import { redirect } from "next/navigation";
+
 export default function Home() {
+  function handleProjectClick() {
+    redirect("/projects");
+  }
   return (
     <main className="flex items-center justify-between ">
       <h1>Hello guys , I am sayyad mahammad</h1>
+      {/* <button onClick={handleProjectClick}>projects</button> */}
     </main>
   );
 }
---
+++ app/projects/page.js
@@ -0,0 +1,3 @@
+export default function Project() {
+  return <h1>this are my projects</h1>;
+}
