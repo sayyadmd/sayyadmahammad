5faf6b5e201022a932eabb6513d25836699aa2ae
Sayyad Mahammad <sayyadmahammad@aesl.in>
2024-07-19 22:58:24 +0530

    add client side comp

--- app/page.js
+++ app/page.js
@@ -1,9 +1,11 @@
 // import HomePage from "@/components/home/HomePage";
+import LikeButton from "./like-button";
 export default function Home() {
   return (
     <main className="w-full h-full bg-[#000] text-[#fff] ">
       <h1>Hello guys , I am sayyad mahammad</h1>
       {/* <HomePage /> */}
+      <LikeButton />
     </main>
   );
 }
