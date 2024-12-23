c65a17363893a89c66de81d8de0e91bac2ad52af
Sayyad Mahammad <sayyadmahammad@aesl.in>
2024-12-23 23:06:18 +0530

    layout fixes

--- app/page.js
+++ app/page.js
@@ -1,5 +1,5 @@
 "use client";
-export default function Home() {
+export default function Page() {
   return (
     <main className="w-full h-full bg-[#0000e6] ">
       <h1>Hellooooooooooooooooo</h1>
--- components/layout/Layout.js
+++ components/layout/Layout.js
@@ -2,9 +2,15 @@ import { TopBar, LeftSideBar } from "./index";
 export default function Layout({ children }) {
   return (
     <div className=" w-screen h-screen sm:h-dvh bg-slate-100 overflow-hidden">
-      <TopBar />
-      <LeftSideBar />
-      {children}
+      <div className="w-full h-24 bg-[#ff5733] ">
+        <TopBar />
+      </div>
+      <div className="flex w-full h-full">
+        <div className="w-20 bg-[#d1cf27] ">
+          <LeftSideBar />
+        </div>
+        <div className="w-full">{children}</div>
+      </div>
     </div>
   );
 }
